368d87cc33c468ea1854c252a1c9c0efe0f5a359
Mohit Hingorani <110980594+mohithingorani@users.noreply.github.com>
2025-10-25 07:49:44 +0530

    Update page.tsx

--- src/app/accuracy/page.tsx
+++ src/app/accuracy/page.tsx
@@ -4,6 +4,7 @@ import { useSearchParams } from "next/navigation";
 export default function Accuracy() {
   const searchParams = useSearchParams();
   const score = searchParams.get("score");
+  if (!score) return <div>No score provided.</div>;
   return (
     <div className="max-w-lg flex justify-center items-center">
       <div className="flex flex-col pt-8">
